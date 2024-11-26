3ba3a1dce8c3b500557b6408d486484283cf7a99
Emirhan Kızmaz <133758059+KizmazEmirhan@users.noreply.github.com>
2024-11-26 10:21:29 +0300

    Update HeaderCategories.vue

--- e-commerce/src/components/UI/HeaderCategories.vue
+++ e-commerce/src/components/UI/HeaderCategories.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="flex justify-center bg-[#FAFAFA] py-4">
+  <div class="flex justify-center bg-[#FAFAFA]">
     <div class="flex container">
       <ul class="flex gap-5 w-full justify-between">
-        <li class="font-bold px-4 py-2 w-full text-center">Item 1</li>
-        <li class="font-bold px-4 py-2 w-full text-center">Item 2</li>
-        <li class="font-bold px-4 py-2 w-full text-center">Item 3</li>
-        <li class="font-bold px-4 py-2 w-full text-center">Item 4</li>
-        <li class="font-bold px-4 py-2 w-full text-center">Item 5</li>
-        <li class="font-bold px-4 py-2 w-full text-center">Item 6</li>
-        <li class="font-bold px-4 py-2 w-full text-center">Item 7</li>
-        <li class="font-bold px-4 py-2 w-full text-center">Item 8</li>
+        <li
+          class="font-bold px-4 w-full text-center hover:text-[#23A6F0] hover:bg-white cursor-pointer py-4"
+          v-for="(category_name, index) in receivedCategories.category_name"
+          :key="index"
+        >
+          {{ category_name }}
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import { db } from "@/firebase";
+import { collection, getDocs } from "firebase/firestore";
+export default {
+  data() {
+    return {
+      receivedCategories: [],
+    };
+  },
+  async created() {
+    try {
+      const querySnapshot = await getDocs(collection(db, "main_categories"));
+      querySnapshot.forEach((doc) => {
+        this.receivedCategories = doc.data();
+      });
+      console.log(this.receivedCategories.category_name);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
+</script>
